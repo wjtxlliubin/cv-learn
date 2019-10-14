2666d7f6602abc95f398eec64f75778a894fac09
stupidmo <2419158326@qq.com>
2019-10-14 23:25:14 +0800

    'add'

--- LessonOne/lesson1.py
+++ LessonOne/lesson1.py
@@ -22,82 +22,82 @@ from matplotlib import pyplot as plt
 # import cv2
 #
 img = cv2.imread('lenna.jpg')
-# cv2.imshow('lenna', img)
-# key = cv2.waitKey()
-# if key == 27:
-#     cv2.destroyAllWindows()
-# # to show color image to show channels
-# print(img)
+cv2.imshow('lenna', img)
+key = cv2.waitKey()
+if key == 27:
+    cv2.destroyAllWindows()
+# to show color image to show channels
+print(type(img))
 # print(img.shape)  # h, w, c
 
 # image crop
-img_crop = img[0:100, 0:200]
+# img_crop = img[0:100, 0:200]
 # cv2.imshow('img_crop', img_crop)
 # key = cv2.waitKey()
 # if key == 27:
 #     cv2.destroyAllWindows()
 
 # color split
-B, G, R = cv2.split(img)
-cv2.imshow('B', B)
-cv2.imshow('G', G)
-cv2.imshow('R', R)
-key = cv2.waitKey()
-if key == 27:
-    cv2.destroyAllWindows()
-
-
-# # change color
-# def random_light_color(img):
-#     # brightness
-#     B, G, R = cv2.split(img)
-#
-#     b_rand = random.randint(-50, 50)
-#     if b_rand == 0:
-#         pass
-#     elif b_rand > 0:
-#         lim = 255 - b_rand
-#         B[B > lim] = 255
-#         B[B <= lim] = (b_rand + B[B <= lim]).astype(img.dtype)
-#     elif b_rand < 0:
-#         lim = 0 - b_rand
-#         B[B < lim] = 0
-#         B[B >= lim] = (b_rand + B[B >= lim]).astype(img.dtype)
-#
-#     g_rand = random.randint(-50, 50)
-#     if g_rand == 0:
-#         pass
-#     elif g_rand > 0:
-#         lim = 255 - g_rand
-#         G[G > lim] = 255
-#         G[G <= lim] = (g_rand + G[G <= lim]).astype(img.dtype)
-#     elif g_rand < 0:
-#         lim = 0 - g_rand
-#         G[G < lim] = 0
-#         G[G >= lim] = (g_rand + G[G >= lim]).astype(img.dtype)
-#
-#     r_rand = random.randint(-50, 50)
-#     if r_rand == 0:
-#         pass
-#     elif r_rand > 0:
-#         lim = 255 - r_rand
-#         R[R > lim] = 255
-#         R[R <= lim] = (r_rand + R[R <= lim]).astype(img.dtype)
-#     elif r_rand < 0:
-#         lim = 0 - r_rand
-#         R[R < lim] = 0
-#         R[R >= lim] = (r_rand + R[R >= lim]).astype(img.dtype)
-#
-#     img_merge = cv2.merge((B, G, R))
-#     # img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
-#     return img_merge
-#
-#
-# img_random_color = random_light_color(img)
-# cv2.imshow('img_random_color', img_random_color)
+# B, G, R = cv2.split(img)
+# cv2.imshow('B', B)
+# cv2.imshow('G', G)
+# cv2.imshow('R', R)
 # key = cv2.waitKey()
 # if key == 27:
 #     cv2.destroyAllWindows()
+
+
+# # change color
+def random_light_color(img):
+    # brightness
+    B, G, R = cv2.split(img)
+
+    b_rand = random.randint(-50, 50)
+    if b_rand == 0:
+        pass
+    elif b_rand > 0:
+        lim = 255 - b_rand
+        B[B > lim] = 255
+        B[B <= lim] = (b_rand + B[B <= lim]).astype(img.dtype)
+    elif b_rand < 0:
+        lim = 0 - b_rand
+        B[B < lim] = 0
+        B[B >= lim] = (b_rand + B[B >= lim]).astype(img.dtype)
+
+    g_rand = random.randint(-50, 50)
+    if g_rand == 0:
+        pass
+    elif g_rand > 0:
+        lim = 255 - g_rand
+        G[G > lim] = 255
+        G[G <= lim] = (g_rand + G[G <= lim]).astype(img.dtype)
+    elif g_rand < 0:
+        lim = 0 - g_rand
+        G[G < lim] = 0
+        G[G >= lim] = (g_rand + G[G >= lim]).astype(img.dtype)
+
+    r_rand = random.randint(-50, 50)
+    if r_rand == 0:
+        pass
+    elif r_rand > 0:
+        lim = 255 - r_rand
+        R[R > lim] = 255
+        R[R <= lim] = (r_rand + R[R <= lim]).astype(img.dtype)
+    elif r_rand < 0:
+        lim = 0 - r_rand
+        R[R < lim] = 0
+        R[R >= lim] = (r_rand + R[R >= lim]).astype(img.dtype)
+
+    img_merge = cv2.merge((B, G, R))
+    # img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+    return img_merge
+
+
+img_random_color = random_light_color(img)
+cv2.imshow('img_random_color', img_random_color)
+key = cv2.waitKey()
+if key == 27:
+    cv2.destroyAllWindows()
 #
 # ###############################
 # # gamma correction
